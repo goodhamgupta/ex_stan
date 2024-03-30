59a34c747a85037579a5bc0a8cc4c44b7c9bc900
goodhamgupta <shubhamg2208@live.com>
2024-03-30 13:05:40 +0800

    Add package docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule ExStan.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo_url "https://github.com/nx-probability/ex_stan/"
+
   def project do
     [
       app: :ex_stan,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: "ExStan, a Elixir Interface to Stan, a platform for statistical modelling and high-performance computation.",
+
+      # Docs
+      name: "ExStan",
+      docs: docs()
     ]
   end
 
@@ -25,6 +36,21 @@ defmodule ExStan.MixProject do
       {:jason, "~> 1.4"},
       {:req, "~> 0.4.0"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["ISC"],
+      links: %{"GitHub" => @repo_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ExStan",
+      source_ref: "v#{@version}",
+      source_url: @repo_url
     ]
   end
 end
