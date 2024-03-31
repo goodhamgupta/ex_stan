eb655198099252aba217208f72958f52f0a74518
goodhamgupta <shubhamg2208@live.com>
2024-04-01 00:34:36 +0800

    Refactor sample function

--- lib/ex_stan/fit.ex
+++ lib/ex_stan/fit.ex
@@ -31,86 +31,126 @@ defmodule ExStan.Fit do
 
   defp validate(obj), do: obj
 
-  defp parse_draws(%Fit{
-         stan_outputs: stan_outputs,
-         num_samples: num_samples,
-         num_chains: num_chains,
-         constrained_param_names: constrained_param_names
-       }) do
-    draws = %{}
+  defp parse_draws(
+         %Fit{
+           stan_outputs: stan_outputs,
+           num_samples: num_samples,
+           num_chains: num_chains,
+           constrained_param_names: constrained_param_names
+         } = fit
+       ) do
+    result = do_parse_draws(stan_outputs, num_samples, num_chains, constrained_param_names)
 
-    stan_outputs
-    |> Enum.with_index()
-    |> Enum.reduce(stan_outputs, fn {stan_output, chain_index}, acc ->
-      draw_index = 0
+    Map.merge(fit, %{
+      _draws: result.draws,
+      sample_and_sampler_param_names: result.sample_and_sampler_param_names
+    })
+  end
 
-      stan_output
-      |> String.split("\n")
-      |> Enum.reduce(acc, fn line, acc ->
-        msg =
-          try do
-            Jason.decode!(line)
-          rescue
-            error ->
-              IO.puts("Error: #{inspect(error)}")
-              IO.puts("Line: #{line}")
-              %{}
-          end
-
-        if Map.get(msg, "topic") == "sample" do
-          values = Map.get(msg, "values")
-
-          acc =
-            if not is_map(values) do
-              acc
-            else
-              acc =
-                if draws == %{} do
-                  feature_names = Map.keys(values)
-
-                  sample_and_sampler_param_names =
-                    Enum.filter(feature_names, fn name -> String.ends_with?(name, "__") end)
-
-                  num_rows =
-                    length(sample_and_sampler_param_names) + length(constrained_param_names)
-
-                  Map.merge(
-                    draws,
-                    %{
-                      _draws: Nx.broadcast(0, {num_rows, num_samples, num_chains}),
-                      sample_and_sampler_param_names: sample_and_sampler_param_names
-                    }
-                  )
-
-                  # if length(constrained_param_names) > 0 and
-                  #      String.ends_with?(List.last(feature_names), "__") do
-                  #   raise "Expected last parameter name to be one declared in program code, found `#{List.last(feature_names)}`"
-                  # end
-                end
-
-              draw_row = values |> Map.values() |> Nx.tensor()
-              {shape} = Nx.shape(draw_row)
-
-              indices =
-                for i <- 0..(shape - 1) do
-                  [i, draw_index, chain_index]
-                end
-                |> Nx.tensor()
-
-              tmp = Nx.indexed_put(acc._draws, indices, draw_row)
-
-              Map.merge(
-                acc,
-                %{_draws: tmp}
-              )
-            end
-
-          acc
-        else
-          acc
-        end
-      end)
+  defp do_parse_draws(stan_outputs, num_samples, num_chains, constrained_param_names) do
+    Enum.with_index(stan_outputs)
+    |> Enum.reduce(nil, fn {stan_output, chain_index}, acc ->
+      parse_stan_output(
+        stan_output,
+        chain_index,
+        acc,
+        num_samples,
+        num_chains,
+        constrained_param_names
+      )
     end)
+  end
+
+  defp parse_stan_output(
+         stan_output,
+         chain_index,
+         acc,
+         num_samples,
+         num_chains,
+         constrained_param_names
+       ) do
+    String.split(stan_output, "\n")
+    |> Enum.reduce(
+      {acc, 0},
+      &process_line(&1, &2, chain_index, num_samples, num_chains, constrained_param_names)
+    )
+    |> elem(0)
+  end
+
+  defp process_line(
+         line,
+         {acc, draw_index},
+         chain_index,
+         num_samples,
+         num_chains,
+         constrained_param_names
+       ) do
+    msg = decode_line(line)
+
+    if Map.get(msg, "topic") == "sample" do
+      process_sample(
+        msg,
+        acc,
+        draw_index,
+        chain_index,
+        num_samples,
+        num_chains,
+        constrained_param_names
+      )
+    else
+      {acc, draw_index}
+    end
+  end
+
+  defp decode_line(""), do: %{}
+  defp decode_line(line), do: Jason.decode!(line)
+
+  defp process_sample(
+         msg,
+         acc,
+         draw_index,
+         chain_index,
+         num_samples,
+         num_chains,
+         constrained_param_names
+       ) do
+    values = Map.get(msg, "values")
+
+    if not is_map(values) do
+      {acc, draw_index}
+    else
+      acc = initialize_acc_if_nil(acc, values, num_samples, num_chains, constrained_param_names)
+      update_draws(acc, values, draw_index, chain_index)
+    end
+  end
+
+  defp initialize_acc_if_nil(nil, values, num_samples, num_chains, constrained_param_names) do
+    feature_names = Map.keys(values)
+    sample_and_sampler_param_names = Enum.filter(feature_names, &String.ends_with?(&1, "__"))
+    num_rows = length(sample_and_sampler_param_names) + length(constrained_param_names)
+
+    %{
+      draws: Nx.broadcast(0, {num_rows, num_samples, num_chains}),
+      sample_and_sampler_param_names: sample_and_sampler_param_names
+    }
+  end
+
+  defp initialize_acc_if_nil(acc, _values, _num_samples, _num_chains, _constrained_param_names),
+    do: acc
+
+  defp update_draws(acc, values, draw_index, chain_index) do
+    draw_row = values |> Map.values() |> Nx.tensor()
+    {shape} = Nx.shape(draw_row)
+    indices = create_indices(shape, draw_index, chain_index)
+    tmp = Nx.indexed_put(acc.draws, indices, draw_row)
+    {%{acc | draws: tmp}, draw_index + 1}
+  end
+
+  defp create_indices(shape, draw_index, chain_index) do
+    for i <- 0..(shape - 1) do
+      [i, draw_index, chain_index]
+    end
+    |> Nx.tensor()
   end
 
   def new(opts) do
