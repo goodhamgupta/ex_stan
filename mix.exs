b00b99fa8ad8f76c1e29549e8e71c5cb5165bac7
goodhamgupta <shubhamg2208@live.com>
2024-04-16 13:23:22 +0800

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExStan.MixProject do
   def project do
     [
       app: :ex_stan,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
